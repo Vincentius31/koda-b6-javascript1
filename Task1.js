8a271395329fec45c5f4ae89731d6b7f3fb162f9
Vincentius Dwi Prasetyo <62978348+Vincentius31@users.noreply.github.com>
2025-12-24 11:31:28 +0700

    Add files via upload

--- Task1.js
+++ Task1.js
@@ -1,8 +1,8 @@
 let jariJari = 7
 const phi = 3.14
 
-let luasLingkaran = phi * jariJari * jariJari
-let kelilingLingkaran = 2 * phi * jariJari
+const luasLingkaran = phi * jariJari * jariJari
+const kelilingLingkaran = 2 * phi * jariJari
 
 let hasilLuasLingkaran = `Hasil Luas Lingkaran: ${luasLingkaran}`
 let hasilKelilingLingkaran = `Hasil Keliling Lingkaran: ${kelilingLingkaran}`
