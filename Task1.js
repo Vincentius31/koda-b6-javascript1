ff0504db595eddf61be2467c89b48029d75ba5b0
Vincentius Dwi Prasetyo <62978348+Vincentius31@users.noreply.github.com>
2025-12-24 10:43:35 +0700

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,8 @@
+const phi = 3.14
+let jariJari = 7
+
+let luasLingkaran = phi * jariJari * jariJari
+let kelilingLingkaran = 2 * phi * jariJari
+
+console.log(luasLingkaran)
+console.log(kelilingLingkaran)
