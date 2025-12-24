c4b7d052c9900444785428e1376e136cf1203d14
Vincentius Dwi Prasetyo <62978348+Vincentius31@users.noreply.github.com>
2025-12-24 14:12:30 +0700

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,11 @@
+const hello = { world: "Hello World"}
+const we = { are: {the: {best: "Koda"}}}
+const obj = {str: [0,1,2,[0,[0,1,{man:[{tech:{academy: "Tech Academy"}}]}]]]}
+const my = [{favourite:[0,1,{fruit: {is: "Apple"}}]}]
+
+console.log(we.are.the.best)
+console.log(hello.world)
+console.log(obj.str[3][1][2].man[0].tech.academy)
+console.log(my[0].favourite[3].fruit.is)
+
+
